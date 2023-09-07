dc6136f8094ca4575e659c7caa8229f5210cc4c6
Aminadab23 <yeabman32@gmail.com>
2023-09-07 15:34:34 +0300

    ...

--- medtalents/src/components/SearchJob.jsx
+++ medtalents/src/components/SearchJob.jsx
@@ -10,7 +10,7 @@ const SearchJob = () => {
   return (
     <div className='border rounded-lg w-fit p-5 h-96'>
     <div className='flex m-5'>
-        <input type="text" placeholder='Search job' className='border rounded-md px-3'/><span><BsSearch size={28} className='border rounded-md cursor-pointer hover:bg-slate-400'/></span>
+        <input type="text" placeholder='Search job' className='border rounded-md px-3'/><span><BsSearch size={28} className='border rounded-md cursor-pointer hover:bg-slate-100'/></span>
     </div>
     <div className='py-5'>
         <div className='flex'>
